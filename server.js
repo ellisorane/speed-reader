640474718d29c4366c7d22601c7e3a3c0a2930dc
ellisorane <ellisorane@yahoo.com>
2021-07-11 15:32:24 -0400

    prepare for deployment

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/user', require('./routes/api/user'));
 app.use('/api/texts', require('./routes/api/texts'));
 
+// For Heroku deploy
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
     //Set static folder
