39e0385cee5a4a123b7021b44cd20c9d09bbe86a
ellisorane <ellisorane@yahoo.com>
2021-07-11 14:54:15 -0400

    heroku deployment configs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -8,12 +9,20 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Running....'));
-
 // Define routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/user', require('./routes/api/user'));
 app.use('/api/texts', require('./routes/api/texts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
