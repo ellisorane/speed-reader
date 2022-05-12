8b6d16dce3d33e53e7f98697aa1e6cec0910fac8
unknown <ellisorane@yahoo.com>
2022-05-11 21:40:47 -0400

    Updated Share page heading

--- client/src/components/Share/Share.js
+++ client/src/components/Share/Share.js
@@ -158,7 +158,7 @@ const Share = () => {
             <div className={classes.container}>
                 <h2 className={classes.pageTitle}>Share your experience</h2>
                 <div className={classes.info}>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p>Did you find this application useful? How has the app helped with your reading speed and comprehension? You can share your experience and or feedback on this page. </p>
                 </div>
 
                 <div className={classes.commentContainer}>
